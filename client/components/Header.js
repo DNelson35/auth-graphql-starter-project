ead93044c37c987f287a69b39378ca89f3fa8c6d
Damien Nelson <dhnisaman@gmail.com>
2023-12-10 15:38:22 -0500

    add check for users before return

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import query from '../queries/CurrentUser'
 
 class Header extends Component {
   render() {
+    const { user } = this.props.data
+    console.log(user)
+    if(!user) return <div></div>
+
     return (
       <div>Header</div>
     )
   }
 }
 
-export default Header
+export default graphql(query)(Header) 
