c7ef8700d6c95a90107e8306a1abd7471438eca6
Damien Nelson <dhnisaman@gmail.com>
2023-12-10 19:15:20 -0500

    Add Links and Logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 import query from '../queries/CurrentUser'
+import mutation from '../mutations/Logout'
 
 class Header extends Component {
-  render() {
-    const { user } = this.props.data
-    console.log(user)
-    if(!user) return <div></div>
 
+    onLogoutClick() {
+        this.props.mutate({
+            refetchQueries: [{ query }]
+        })
+    }
+
+    renderButtons() {
+        const { loading, user } = this.props.data
+
+        if(loading) { <div/> }
+
+        if(user){
+            return (
+                <li>
+                    <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+                </li>
+            )
+        }else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
+            )
+        }
+    }
+  render() {
     return (
-      <div>Header</div>
+      <nav>
+        <div className='nav-wrapper'>
+            <Link to='/' className='brand-log left'>
+                Home
+            </Link>
+            <ul className='right'>
+                {this.renderButtons()}
+            </ul>
+        </div>
+      </nav>
     )
   }
 }
 
-export default graphql(query)(Header) 
+export default graphql(mutation)(
+    graphql(query)(Header) 
+)
