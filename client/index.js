ad67929854a062acc14fd7b2904db1ffb4d96791
Damien Nelson <dhnisaman@gmail.com>
2023-12-13 00:17:23 -0500

    import require auth and wrap the dashboard

--- client/index.js
+++ client/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import { Router, hashHistory, Route } from 'react-router';
 import App from './components/App'
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import Dashboard from './components/Dashboard';
+import requireAuth from './components/requireAuth';
 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
@@ -29,7 +30,7 @@ const Root = () => {
         <Route path='/' component={App}>
           <Route path='login' component={LoginForm}/>
           <Route path="signup" component={SignupForm} />
-          <Route path="dashboard" component={Dashboard} />
+          <Route path="dashboard" component={requireAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
