f73de9a781fa78e901af63342dde71c8a6e65736
Damien Nelson <dhnisaman@gmail.com>
2023-12-12 23:47:32 -0500

    add cwu to track changes to the user login status

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,13 +3,25 @@ import AuthForm from './AuthForm'
 import mutation from '../mutations/Login'
 import { graphql } from 'react-apollo'
 import query from '../queries/CurrentUser'
+import { hashHistory } from 'react-router'
+
 
 class LoginForm extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+      super(props)
 
-    this.state = { errors: [] }
-  }
+      this.state = { errors: [] }
+    }
+
+    componentWillUpdate(nextProps){
+      // this.props // old props, current set of props
+      // nextProps // the next set of props that will be in place when the component rerenders
+      if(!this.props.data.user && nextProps.data.user){
+        //redirect to dashboard
+        hashHistory.push('/dashboard')
+      }
+    }
+
     onSubmit({ email, password }) {
         this.props.mutate({
             variables: { email, password},
@@ -31,4 +43,6 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm)
+export default graphql(query)(
+  graphql(mutation)(LoginForm)
+)
